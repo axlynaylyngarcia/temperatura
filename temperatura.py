0ab488ee1aaae82e2d4ffedd0fec52d54e9821a2
axlynaylyngarcia <axlyngarcia0201@gmail.com>
2022-04-22 06:02:56 -0500

    Desarrollo: crecion diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,18 @@
+"Programa No. 3"
+"Programa para calcular grados kelvin y celcius"
+
+print("-------------------------")
+print("----grados celcius a farenheit y a kelvin----")
+print("-------------------------")
+
+#input
+
+c=int(input("digite los grados"))
+
+#processing
+k=c+273.15
+f=c*1.8+32
+
+#ouput
+print(str(c) + " grados celcius son " + str(k)+ " grados kelvin y " + str(f)+ " grados farenheit ") 
+ 
